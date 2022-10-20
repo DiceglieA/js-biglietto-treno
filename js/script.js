a0f768e20ffb68ad6698956a56d9bdc4b6dbab11
DiceglieA <angelodiceglie10@gmail.com>
2022-10-20 14:36:58 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-const pricekm = '0.21'
+const priceKm = '0.21'
 let km = parseInt(prompt('quanti chilometri dovrai percorrere?'))
 //console.log('km')
 let age = parseInt(prompt('quanti anni hai?'))
 //console.log('age')
-let price = km * pricekm;
+let price = km * priceKm;
 //console.log('price')
+
+if (age <= 18) {
+    price = price * 0.8
+}
+else if (age >= 65) {
+    price = price * 0.6
+}
+else {
+    cost = price
+}
+
+document.getElementById('cost').innerHTML = `Il prezzo del biglietto è: ${price.toFixed(2)}`
