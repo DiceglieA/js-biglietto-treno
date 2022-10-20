38ce306bb54c436414eff658993472d0e003f9a5
DiceglieA <angelodiceglie10@gmail.com>
2022-10-20 13:57:34 +0200

    determinate variabili

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const pricekm = '0.21'
+let km = parseInt(prompt('quanti chilometri dovrai percorrere?'))
+//console.log('km')
+let age = parseInt(prompt('quanti anni hai?'))
+//console.log('age')
+let price = km * pricekm;
+//console.log('price')
